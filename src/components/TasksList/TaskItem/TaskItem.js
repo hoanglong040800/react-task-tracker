bb450af2f7d6e17458988cc5a84c1ebc62f16c54
hoanglong040800 <hoanglong040800@gmail.com>
2021-02-06 09:23:02 +0700

    fix style task item

--- src/components/TasksList/TaskItem/TaskItem.js
+++ src/components/TasksList/TaskItem/TaskItem.js
@@ -4,32 +4,38 @@ import { BsAlarmFill, BsAlarm } from "react-icons/bs"
 
 const TaskItem = ({ task, onDelete, onToggle }) => {
   return (
-    <div className='task-item-container'>
 
-      <div className="task-item__text">
-        <h4 className='task-item'>{task.text}</h4>
-        <p className='task-item'>{task.time}</p>
-        {
-          task.reminder
-            ? (
-              <BsAlarmFill
-                className='icon-alarm'
-                onClick={() => onToggle(task.id)}
-              />
-            ) : (
-              <BsAlarm
-                className='icon-alarm'
-                onClick={() => onToggle(task.id)}
-              />
-            )
-        }
+    <div className='task-item__container' >
+
+      {/* Reminder */}
+      <div
+        className="task-item__reminder"
+        onClick={() => onToggle(task.id)}
+        style={{
+          backgroundColor: task.reminder ? 'var(--primary-color)' : 'lightgray',
+          color: task.reminder ? '#fff' : '#333',
+        }}
+      >
+        <BsAlarm
+          className='icon-alarm'
+        />
       </div>
 
-      <TiDelete
-        className='icon-del'
-        onClick={() => onDelete(task.id, task.text)}
-      />
+      {/* Main Content */}
+      <div className='task-item__main'>
+        <div className="task-item__text">
+          <h4 className='task-item'>{task.text}</h4>
+          <p className='task-item'>{task.time}</p>
+        </div>
+
+        <TiDelete
+          className='icon-del'
+          onClick={() => onDelete(task.id, task.text)}
+        />
+      </div>
+
     </div>
+
   )
 }
 
